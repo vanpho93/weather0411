ecf47573311d2e151911cf2c6ebb151c0d51033f
Van Pho Nguyen <vanpho02@gmail.com>
2017-11-18 16:03:53 +0700

    xong

--- src/components/WeatherForm.js
+++ src/components/WeatherForm.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+
+const URL = 'http://api.openweathermap.org/data/2.5/weather?appid=01cc37655736835b0b75f2b395737694&units=metric&q=';
 
 export default class WeatherForm extends Component {
     constructor(props) {
@@ -10,9 +13,16 @@ export default class WeatherForm extends Component {
     }
 
     getWeather() {
-        // console.log(this.state.txtCityName);
-        this.props.onSetWeather(this.state.txtCityName, 30);
-        this.setState({ txtCityName: '' });
+        axios.get(URL + this.state.txtCityName)   
+        .then(res => {
+            this.props.onSetWeather(this.state.txtCityName, res.data.main.temp);
+            this.setState({ txtCityName: '' });
+        })
+        .catch(err => {
+            alert(err.response.data.message);
+            this.setState({ txtCityName: '' });
+        });
+        // this.props.onSetWeather(this.state.txtCityName, 30);
     }
 
     render() {
